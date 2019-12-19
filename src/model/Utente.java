550f793cc230f7f5fb9a8a3ab5bd49e7189e6727
nikocostantino <niko97142@gmail.com>
2019-12-19 17:31:38 +0100

    hghjg

--- src/model/Utente.java
+++ src/model/Utente.java
@@ -1,5 +1,34 @@
 package model;
 
 public class Utente {
-
+	String nome;
+	String cognome;
+	String password;
+	String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
