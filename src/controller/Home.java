b5d3ceda9a6ee5b309c5a2ebda0ad89b62f9c9ab
nikocostantino <niko97142@gmail.com>
2019-12-20 21:02:25 +0100

    sono riuscito a fare funzionare login e logout

--- src/controller/Home.java
+++ src/controller/Home.java
@@ -11,13 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 public class Home extends HttpServlet {
 		@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-<<<<<<< HEAD
-			System.out.println("ciao");
+
+			System.out.println("ciaoooooo");
 			RequestDispatcher rd = req.getRequestDispatcher("html/home.html");
-=======
-			System.out.println("sono nella do get di home");
-			RequestDispatcher rd = req.getRequestDispatcher("home2.html");
->>>>>>> branch 'master' of https://github.com/nikocostantino/Allenati_al_VAR
 			rd.forward(req, resp);
 		}
 		
--- src/controller/Login.java
+++ src/controller/Login.java
@@ -18,35 +18,18 @@ public class Login extends HttpServlet{
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			System.out.println("sono qui GET");
 
-			Object utente = req.getSession().getAttribute("utente");
-			String isLogout = req.getParameter("logout");
-			if (isLogout != null && isLogout.equals("true")) {
-				req.getSession().removeAttribute("utente");
-				RequestDispatcher rd = req.getRequestDispatcher("/home");
+				RequestDispatcher rd = req.getRequestDispatcher("/index");
 				rd.forward(req, resp);
-			}else {
-				if (utente == null) {
-					RequestDispatcher rd = req.getRequestDispatcher("/html/index.html");
-					rd.forward(req, resp);
-				}
-			}
 		}
 		
 		@Override
 		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			String email = req.getParameter("email");
 			String password = req.getParameter("password");
-			System.out.println("sono quiPOST");
 			Utente utente = DBManager.getInstance().login(email, password);
 			if (utente != null) {
 				req.getSession().setAttribute("utente", utente);
-//				resp.sendRedirect(".");
-				/*
-				ServletContext context = getServletContext().getContext("package name of other servlet");
-		        RequestDispatcher rd = context.getRequestDispatcher("/name of servlet");
-		        rd.forward(request, response);
-		        */
-				RequestDispatcher rd = req.getRequestDispatcher("");
+				RequestDispatcher rd = req.getRequestDispatcher("/home");
 				rd.forward(req, resp);
 			}else {
 				RequestDispatcher rd = req.getRequestDispatcher("/html/error_page.html");
