b860da871cb83bd6dd16bc96f256eb1f0fb77674
Simone <simonecimino20@gmail.com>
2019-12-20 15:09:30 +0100

    login

---
+++ src/controller/Login.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Utente;
+import persistence.DBManager;
+
+
+public class Login extends HttpServlet{
+	
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			System.out.println("sono qui");
+
+			Object utente = req.getSession().getAttribute("utente");
+			String isLogout = req.getParameter("logout");
+			if (isLogout != null && isLogout.equals("true")) {
+				req.getSession().removeAttribute("utente");
+				RequestDispatcher rd = req.getRequestDispatcher("");
+				rd.forward(req, resp);
+			}else {
+				if (utente == null) {
+					RequestDispatcher rd = req.getRequestDispatcher("/html/index.html");
+					rd.forward(req, resp);
+				}
+			}
+		}
+		
+		@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			String email = req.getParameter("email");
+			String password = req.getParameter("password");
+			System.out.println("sono qui");
+			Utente utente = DBManager.getInstance().login(email, password);
+			if (utente != null) {
+				req.getSession().setAttribute("utente", utente);
+//				resp.sendRedirect(".");
+				
+				RequestDispatcher rd = req.getRequestDispatcher("");
+				rd.forward(req, resp);
+			}else {
+				RequestDispatcher rd = req.getRequestDispatcher("/html/error_page.html");
+				rd.forward(req, resp);
+			}
+			
+		}
+}
